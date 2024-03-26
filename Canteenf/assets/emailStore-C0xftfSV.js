6bc4444018e9740b08a7aedad609222179a0f348
lains <ling3212045218@gmail.com>
2024-03-26 20:43:49 +0800

    1

--- Canteenf/assets/emailStore-C0xftfSV.js
+++
@@ -1,1 +0,0 @@
-import{C as t}from"./index-Cry4P2XB.js";const i=t("emailAddress",{state:()=>({email:""}),getters:{getEmail:e=>e.email},actions:{setEmail(e){this.email=e}}});export{i as u};
---
+++ Canteenf/assets/emailStore-CTxKOj7e.js
@@ -0,0 +1,1 @@
+import{C as t}from"./index-Cni_yBT1.js";const i=t("emailAddress",{state:()=>({email:""}),getters:{getEmail:e=>e.email},actions:{setEmail(e){this.email=e}}});export{i as u};
