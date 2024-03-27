92e78adfb2de21e6394babf92ef9d600c3feb8d2
lains <ling3212045218@gmail.com>
2024-03-27 14:46:24 +0800

    1

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,4 @@
-CREATE USER 'lains'@'localhost' IDENTIFIED BY 'M+w!C!acuh1X.M!buO,2';
-CREATE USER 'lains'@'%' IDENTIFIED BY 'M+w!C!acuh1X.M!buO,2';
+CREATE USER 'lains'@'localhost' IDENTIFIED BY 'jQKOVqW0%W!n*cDq2B&2';
+CREATE USER 'lains'@'%' IDENTIFIED BY 'jQKOVqW0%W!n*cDq2B&2';
 GRANT ALL ON *.* TO 'lains'@'localhost';
 GRANT ALL ON *.* TO 'lains'@'%';
