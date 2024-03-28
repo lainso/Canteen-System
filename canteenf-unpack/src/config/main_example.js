0a97e1cfb8be5a4367dc09e66106bbd9546cdaec
lains <ling3212045218@gmail.com>
2024-03-28 13:07:13 +0800

    1

--- canteenf-unpack/src/config/main_example.js
+++ canteenf-unpack/src/config/main_example.js
@@ -1,6 +1,6 @@
 // 定义后端接口URL
-export const baseURL_main = "xxxx"
+export const baseURL_main = "http://xxx.xxx.xxx.xxx:xx"
 
 // 定义前端服务地址和端口
-export const server_host = "xxxx"
+export const server_host = "xxx.xxx.xxx.xxx"
 export const server_port = 80
